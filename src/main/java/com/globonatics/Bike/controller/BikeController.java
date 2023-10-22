0ac9acdb0c78794c40075a2025332840d3f812bf
Sameeh Hussein <sameehhussein02@gmail.com>
2023-10-22 19:02:19 +0300

    Update domain and controller packages

--- src/main/java/com/globonatics/Bike/controller/BikeController.java
+++ src/main/java/com/globonatics/Bike/controller/BikeController.java
@@ -1,2 +1,32 @@
-package com.globonatics.Bike.controller;public class BikeController {
+package com.globonatics.Bike.controller;
+
+import com.globonatics.Bike.domain.Bike;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.OnClose;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/bikes")
+public class BikeController {
+
+    @GetMapping
+    public List<Bike> list(){
+        List<Bike> bikeList = new ArrayList<>();
+        return bikeList;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@PathVariable("id") long id){
+
+    }
+
+    @GetMapping(path = "/{id}")
+    public Bike get(@PathVariable("id") long id){
+        Bike bike = new Bike();
+        return bike;
+    }
 }
--- src/main/java/com/globonatics/Bike/domain/Bike.java
+++ src/main/java/com/globonatics/Bike/domain/Bike.java
@@ -1,2 +1,31 @@
-package com.globonatics.Bike.domain;public class Bike {
+package com.globonatics.Bike.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Bike {
+    @Id
+    @NonNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String model;
+    private String serialNumber;
+    private BigDecimal purchasePrice;
+    private Date purchaseDate;
+    @NonNull
+    private boolean contact;
 }
